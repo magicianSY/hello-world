6cb4c117cb0ca2973eaec29ceb8a14fd517b1b1c
magicianSY <42198443+magicianSY@users.noreply.github.com>
2019-02-07 11:37:10 +0800

    Update equation.cpp
    add a branch structrue to justify if there is a real root

--- equation.cpp
+++ equation.cpp
@@ -4,12 +4,18 @@
 int main(void)
 {	
 	double a,b,c,r1,r2;
-	scanf("%lf %lf %lf",&a,&b,&c);         //wrong form: scanf("%d %d %d",&a, &b, &c);
-	r1=(-b+sqrt(pow(b,2)-4*a*c))/(2*a);    //pow() and sqrt() require a double type
-	r2=(-b-sqrt(pow(b,2)-4*a*c))/(2*a);
+	scanf("%lf %lf %lf",&a,&b,&c);                 //wrong form: scanf("%d %d %d",&a, &b, &c);
+	if (sqrt(pow(b,2)-4*a*c)>0) {                  //pow() and sqrt() require a double type
+		r1=(-b+sqrt(pow(b,2)-4*a*c))/(2*a);    //calculate the roots
+		r2=(-b-sqrt(pow(b,2)-4*a*c))/(2*a);
+		printf("r1=%7.2f\n",r1);
+		printf("r2=%7.2f\n",r2);
+	}
+	 else {
+		printf("No real roots!");              //if delta<0, print "No real root!"
+	 }
 	
-	printf("r1=%7.2f\n",r1);
-	printf("r2=%7.2f\n",r2);
+
 	
 	
 	return 0;
