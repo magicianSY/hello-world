c0c6f788979a3ef9937126137ce7a5ce2d0323fe
magicianSY <42198443+magicianSY@users.noreply.github.com>
2019-02-07 10:56:33 +0800

    Add files via upload
    type of arguments of a function should be considered

---
+++ equation.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h> 
+int main(void)
+{	
+	double a,b,c,r1,r2;
+	scanf("%lf %lf %lf",&a,&b,&c);
+	r1=(-b+sqrt(pow(b,2)-4*a*c))/(2*a);
+	r2=(-b-sqrt(pow(b,2)-4*a*c))/(2*a);
+	
+	printf("r1=%7.2f\n",r1);
+	printf("r2=%7.2f\n",r2);
+	
+	
+	return 0;
+ } 
