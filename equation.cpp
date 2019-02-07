f2909625a2923e632c92dffc0f509598bbd1fabb
magicianSY <42198443+magicianSY@users.noreply.github.com>
2019-02-07 11:00:14 +0800

    Update equation.cpp
    add some description of mistake that i have made

--- equation.cpp
+++ equation.cpp
@@ -1,10 +1,11 @@
+//quadratic equation of one variable.c
 #include <stdio.h>
 #include <math.h> 
 int main(void)
 {	
 	double a,b,c,r1,r2;
-	scanf("%lf %lf %lf",&a,&b,&c);
-	r1=(-b+sqrt(pow(b,2)-4*a*c))/(2*a);
+	scanf("%lf %lf %lf",&a,&b,&c);         //wrong form: scanf("%d %d %d",&a, &b, &c);
+	r1=(-b+sqrt(pow(b,2)-4*a*c))/(2*a);    //pow() and sqrt() require a double type
 	r2=(-b-sqrt(pow(b,2)-4*a*c))/(2*a);
 	
 	printf("r1=%7.2f\n",r1);
