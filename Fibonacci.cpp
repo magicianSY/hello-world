7f3a31045a6466090cbedae72a48a84176f1712a
magicianSY <42198443+magicianSY@users.noreply.github.com>
2019-02-09 21:51:46 +0800

    add some comments

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -3,12 +3,12 @@
 int main(void)
 {
 	int  n,a1, a2, a3;
-	a1=1; 
-	a2=1;
-	a3=1;
+	a1=1;                              //the (n-2)th item of the sequence
+	a2=1;                              //the (n-1)th item of the sequence 
+	a3=1;                              //the nth item of the sequence 
 	scanf("%d", &n);
 	
-	if (n==1)
+	if (n==1)                           //the former mistake: mix "="&"==" 
 		printf("%d", a1);
 	 else {
 		 for (int i=1; i<n; i++) {
@@ -20,9 +20,5 @@ int main(void)
 		printf("%d\n", a1);
 	}
 		
-	 
-		
-	
-	
 	return 0;
  } 
