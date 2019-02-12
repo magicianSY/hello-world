cf35fab29acf8795b3d65f01c2adfb48849d694a
magicianSY <42198443+magicianSY@users.noreply.github.com>
2019-02-12 18:24:35 +0800

    add some description
    i am so stupid to spend that much time on this problem

--- insertion sorting .cpp
+++ insertion sorting .cpp
@@ -1,3 +1,4 @@
+//insertion sorting.cpp  insert a given number into an asceneded array
 #include<stdio.h>
 #define LENGTH 10
 int main(void)
@@ -5,18 +6,18 @@ int main(void)
 	int temp1,temp2,i,j,m;
 	int a[10];
 	
-	for (i=0; i<LENGTH-1; i++) 
-		scanf("%d",&a[i]);
-	scanf("%d",&m);
+	for (i=0; i<LENGTH-1; i++)                             //read nine ordered number
+		scanf("%d",&a[i]);                             
+	scanf("%d",&m);                                        //read the given number
 	
 	if (m>a[LENGTH-2])
-		a[LENGTH-1]=m;
+		a[LENGTH-1]=m;                                //if m=max(a[i]),then put it in the last place
 	 else {
-	 	for (i=0;i<LENGTH; i++) {
-	 		if (m<a[i]) {
+	 	for (i=0;i<LENGTH; i++) {                     //compare the given number with each items of the array 
+	 		if (m<a[i]) {                         //find the items that exactly smaller than the given number,then exchange
 	 			temp1=a[i];
 	 			a[i]=m;
-	 			for (j=i+1; j<LENGTH; j++) {
+	 			for (j=i+1; j<LENGTH; j++) {   //the following items go one place behind
 	 				temp2=a[j];
 	 				a[j]=temp1;
 	 				temp1=temp2;
@@ -25,6 +26,6 @@ int main(void)
 			 }
 		 }
 	 }
-	for(i=0; i<LENGTH; i++) 
+	for(i=0; i<LENGTH; i++)                               //print the new array
 		printf("%d\n",a[i]);
  } 
