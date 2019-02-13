4b7ae9f85a6e0fc9d4d0f8a5c991c14481e63596
magicianSY <42198443+magicianSY@users.noreply.github.com>
2019-02-13 20:02:03 +0800

    Update yanghui_triangle.cpp
    add some comments

--- yanghui_triangle.cpp
+++ yanghui_triangle.cpp
@@ -8,11 +8,11 @@ int main(void)
 	scanf("%d",&row);
 	for (i=0; i<SIZE; i++) {
 		for (j=0; j<SIZE; j++) {
-		    a[i][j]=0;
+		    a[i][j]=0;                     //i row j column array
 			
 		}
 	}
-	/*for (i=0; i<SIZE; i++) {
+	/*for (i=0; i<SIZE; i++) {                 //this block can print the two dimensional array at present
 		for (j=0; j<SIZE; j++) {
 			printf("%d",a[i][j]);
 		}
@@ -20,24 +20,24 @@ int main(void)
 	}*/
 		
 		
-	for (j=0; j<SIZE; j++)
-		a[j][0]=1;
-	for (i=1; i<SIZE; i++)
+	for (j=0; j<SIZE; j++)                       //change the first column to 1
+		a[j][0]=1;  
+	for (i=1; i<SIZE; i++)                       //change the ith row ith column to 1
 		a[i][i]=1;
 
 
 	for (i=1; i<SIZE; i++) {
 			for (j=1; j<SIZE-1; j++) {
-				a[i][j]=a[i-1][j]+a[i-1][j-1];
+				a[i][j]=a[i-1][j]+a[i-1][j-1];   //the key recursion that represent the rule of yanghui triangle
 			}
 	}
 	
 
 	for (i=0; i<row; i++) {
 		for (j=0; j<=i; j++) {
-			printf("%d ",a[i][j]);
+			printf("%d ",a[i][j]);               //print the triangle
 		}
-			printf("\n");
+			printf("\n");                        //former error: miss the "\n"
 	}	
-	
+	return 0;
  } 
