421564be2de61bb1aeec092304b9a15608d9c43a
magicianSY <42198443+magicianSY@users.noreply.github.com>
2019-02-25 19:35:12 +0800

    add some discripttion

--- struct1.cpp
+++ struct1.cpp
@@ -1,6 +1,7 @@
+//print the highest and the lowest score in the class
 #include <cstdio>
 #define SIZE 100
-struct student {
+struct student {               //define a struct to store the information of each student
 	char name[SIZE];
 	char id[SIZE];
 	int score;
@@ -11,13 +12,13 @@ int main(void)
 	int n;
 	scanf("%d",&n);
 	ans_max.score=0;
-	ans_min.score=101;
+	ans_min.score=101;              //initialize the max and the min.even if there is only one input ,the max and the min would be change into that one.
 	
 	for (int i=0; i<n; i++) {
 		scanf("%s%s%d",&temp.name, &temp.id, &temp.score);
 		
 		if (temp.score>ans_max.score)
-			ans_max=temp;
+			ans_max=temp;                 //copy all three variables in the structure
 		if (temp.score<ans_min.score)
 			ans_min=temp;
 	}	
